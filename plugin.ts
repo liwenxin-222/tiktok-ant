61998c61b11e2e9abec335f41c54b7a2ccf40ac2
李文新 <40284708+liwenxin-222@users.noreply.github.com>
2022-08-24 11:06:01 +0800

    update

--- plugin.ts
+++ plugin.ts
@@ -9,7 +9,11 @@ export default (api: IApi) => {
       `<meta http-equiv="X-UA-Compatible" content="IE=edge"/>`,
       `<meta name="viewport" content="width=device-width, initial-scale=1.0"/>`,
       `<meta name="referrer" content="never"/>`,
-      `<meta name="description" content="download video without water mesk"/>`,
+      // `<meta name="description" content="download video without water mesk"/>`,
+      `<meta name="keywords" content="word转换成pdf转换器,word转换成pdf,在线word转pdf,word转pdf免费" />`,
+      `<meta name="description" content="word转换成pdf转换器为广大用户提供在线word转pdf、word转pdf免费等服务，让你轻松畅游在不同版本word转换pdf中。" />`,
+      `<meta name="keywords" content="抖音小工具,抖音视频下载,抖音视频排序" />`,
+      `<meta name="description" content="抖音视频下载，抖音视频列表排序" />`
     ])
     return $;
   });
